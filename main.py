d744803763e0c3585061f9b4a90c54159659265a
wyawon <100033232+wyawon@users.noreply.github.com>
2022-02-20 00:22:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -35,12 +35,20 @@ async def search(ctx, arg):
 
     if result:
         embed = discord.Embed(title=word, description="I got a special color!", color=discord.Colour.orange())
-        embed.add_field(name="サービス名", value=i[0])
-        embed.add_field(name="ID", value=i[1])
-        embed.add_field(name="管理者", value=i[2])
-        embed.add_field(name="リンク", value=i[3])
-        embed.add_field(name="説明", value=i[4])
-        embed.add_field(name="連携先サービス", value=i[5])
+        embed.add_field(name="図鑑番号", value=i[0])
+        embed.add_field(name="ポケモン名", value=i[1])
+        embed.add_field(name="タイプ１", value=i[2])
+        embed.add_field(name="タイプ２", value=i[3])
+        embed.add_field(name="通常特性１", value=i[4])
+        embed.add_field(name="通常特性２", value=i[5])
+        embed.add_field(name="夢特性", value=i[6])
+        embed.add_field(name="HP", value=i[7])
+        embed.add_field(name="こうげき", value=i[8])
+        embed.add_field(name="ぼうぎょ", value=i[9])
+        embed.add_field(name="とくこう", value=i[10])
+        embed.add_field(name="とくぼう", value=i[11])
+        embed.add_field(name="すばやさ", value=i[12])
+        embed.add_field(name="合計", value=i[13])
         embed.set_footer(text="made by P4sTela",
                          icon_url="https://pbs.twimg.com/profile_images/1387009460581801991/OKa0sdnY_400x400.jpg")
 
